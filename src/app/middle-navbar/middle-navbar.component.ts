dc51dcfa09199713faab4c679c2948b5d7663c55
Horace M Asher <masherdev@gmail.com>
2024-03-19 14:17:52 +0300

    first commit

---
+++ src/app/middle-navbar/middle-navbar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-middle-navbar',
+  templateUrl: './middle-navbar.component.html',
+  styleUrls: ['./middle-navbar.component.scss']
+})
+export class MiddleNavbarComponent {
+
+}
